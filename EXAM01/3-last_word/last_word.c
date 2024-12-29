582072f97ac460da95f95872dd9bd7cc5159320b
mehmetsrepo <mehmetemisevim@gmail.com>
2024-12-29 16:53:33 +0300

    Update last_word.c
    your code was faulty it return the first word in an argument instead of the last one and would return without its first letter if no space or tabs is present before if for example if ı enter "for pony" it would return "or" its the first word for without its first letter and this isn't the intended result ı have committed changes in your code that makes it work as it is intended.

--- EXAM01/3-last_word/last_word.c
+++ EXAM01/3-last_word/last_word.c
@@ -7,8 +7,8 @@ void	last_word(char *str)
 	i -= 1;
 	while(str[i] == '\t' || str[i] == 32)
 		i--;
-	while (i > 0)
-	{	if(str[i] == 32 && str[i] == '\t')
+	while (i >= 0)
+	{	if(str[i] == 32 || str[i] == '\t')
 			break;
 		i--;
 	}
